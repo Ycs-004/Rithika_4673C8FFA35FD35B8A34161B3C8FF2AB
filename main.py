a5cb5531eaaa81e567fe930d307b52e52bced0ff
Ycs-004 <145273831+Ycs-004@users.noreply.github.com>
2023-10-14 19:11:29 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,30 +1,12 @@
-'''implement a class called player that represents a cricket player.
-the play class should have a method called play() which prints" the player is playing cricket
-device two classEdu,batsman and bowler, from the player class.
-override the play()method in each derived class to print" the batsman is batting"and"the bowler is bowling", 
-respectlovely write a program to create objects of booth the batsman create objects of both the batsman and bowler classes and call the play() method for each objects'''
+def linear_search_product(products, target_product):
+    indices = []
+    for index, product in enumerate(products):
+        if product == target_product:
+            indices.append(index)
+    return indices
 
-
-# defined the base class player
-class player:
-  def play(self):
-    print("the player is playing cricket.")
-
-#define the derived class batsman
-class Batsman(player):
-  def play(self):
-    print("the batsman is batting.")
-
-# defined the derived class bowler
-class Bowler(player):
-  def play(self):
-    print("the bowler is bowling")
-
-# create objects of batsman and bowler classEdu
-batsman = Batsman()
-bowler = Bowler()
-
-# call the play() method to each object
-batsman.play()
-bowler.play()
-
+# Example usage:
+products_list = ["product1", "product2", "product3", "product2", "product4", "product2"]
+target_product = "product2"
+result = linear_search_product(products_list, target_product)
+print("Indices of occurrences:", result)
